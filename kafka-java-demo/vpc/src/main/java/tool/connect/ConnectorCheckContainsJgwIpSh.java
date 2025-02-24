9c1b2922cbcee4651d01b020a074e89b7ff4ecf9
lishixiong <15811555789@163.com>
2025-02-24 12:11:42 +0800

    元数据更新

--- kafka-java-demo/vpc/src/main/java/tool/connect/ConnectorCheckContainsJgwIpSh.java
+++ kafka-java-demo/vpc/src/main/java/tool/connect/ConnectorCheckContainsJgwIpSh.java
@@ -118,7 +118,7 @@ public class ConnectorCheckContainsJgwIpSh {
                         }
                     }
                 }
-                Thread.sleep(500);
+                Thread.sleep(100);
             }
         }
     }
