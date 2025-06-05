387aaf8ad9ec9fc0198d8ada56a288f09c15d840
allensunli <allensunli@tencent.com>
2025-06-05 14:45:25 +0800

    hive元数据更新demo

--- kafka-java-demo/vpc/src/main/java/publicnet/HiveMetaStoreClientTest.java
+++ kafka-java-demo/vpc/src/main/java/publicnet/HiveMetaStoreClientTest.java
@@ -3,26 +3,32 @@ package publicnet;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
 import org.apache.hadoop.hive.metastore.api.Database;
-import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.hadoop.security.UserGroupInformation;
 
 import java.util.List;
 
 public class HiveMetaStoreClientTest {
+    //thrift://30.46.110.3:11175
     public static void main(String[] args) {
         try {
+            String uri = args[0];
+            System.out.println("uri:" + uri);
             // 设置 Hadoop 配置
             Configuration conf = new Configuration();
             conf.set("hadoop.security.authentication", "kerberos");
-            conf.set("hive.metastore.uris", "thrift://30.46.110.3:11175"); // 替换为你的 HMS 地址
+            conf.set("hive.metastore.uris", uri); // 替换为你的 HMS 地址
             // 设置 Kerberos 配置
             System.setProperty("java.security.krb5.conf", "/etc/krb5.conf"); // 替换为你的 krb5.conf 路径;
+            System.setProperty("java.security.krb5.conf", "/etc/krb5.conf"); // 替换为你的 krb5.conf 路径;
+
             UserGroupInformation.setConfiguration(conf);
             UserGroupInformation.loginUserFromKeytab("kafka@EMR-S0M9K67E", "/tmp/kafka.keytab"); // 替换为你的 principal 和 keytab
 
             // 创建 HiveMetaStoreClient
             HiveMetaStoreClient client = new HiveMetaStoreClient(conf);
+            client.setMetaConf("hive.metastore.uris", uri); // 替换为你的 HMS 地址
+            client.setMetaConf("hive.metastore.client.capability.check","false");
 
 
             // 获取所有数据库
